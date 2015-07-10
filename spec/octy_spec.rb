46ee802889f5ae3667098170e9b0d678d1109ace
kakakakakku <y.yoshida22@gmail.com>
2015-07-10 20:34:18 +0900

    Update default spec

--- spec/octy_spec.rb
+++ spec/octy_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe Octy do
-  it 'has a version number' do
-    expect(Octy::VERSION).not_to be nil
+
+  describe 'version' do
+    it 'has a version number' do
+      expect(Octy::VERSION).not_to be nil
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
